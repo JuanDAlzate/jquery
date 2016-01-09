2fa9ebfd4aa54cc7b57050375085d42a8550b6c0
JuanDa04 <miamiau.04@hotmail.com>
2016-01-08 20:17:40 -0500

    Agregamos un comentario

--- src/css/addGetHookIf.js
+++ src/css/addGetHookIf.js
@@ -3,6 +3,7 @@ define( function() {
 function addGetHookIf( conditionFn, hookFn ) {
 
 	// Define the hook, we'll check on the first run if it's really needed.
+	//Esto yo lo he editado ...>
 	return {
 		get: function() {
 			if ( conditionFn() ) {
